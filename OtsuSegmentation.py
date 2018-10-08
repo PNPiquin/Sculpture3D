f90eb710382bbda5b25d62e456ac7df0bdacfa9e
Unknown <pierre-nicolas.piquin@mines-paristech.fr>
2018-10-08 19:33:58 +0200

    Fix Otsu segmentation

--- OtsuSegmentation.py
+++ OtsuSegmentation.py
@@ -6,7 +6,17 @@ import matplotlib.pyplot as plt
 def otsu_segmentation(mat):
     min_, max_ = kop.matrix_extremum(mat)
     hist, bins = np.histogram(mat.flatten(), 256, [0, max_])
+
+    # histogram normalization
+    height = len(mat)
+    width = len(mat[0])
+    n_pixels = width * height
+    histogram = []
+    for k in range(256):
+        histogram += [float(hist[k]) / n_pixels]
+
     hist[0] = 0
+    print(histogram)
     plt.hist(mat.flatten(), 256, [0, max_])
     plt.show()
 
@@ -17,18 +27,19 @@ def otsu_segmentation(mat):
         p1_k[k] = 0
         mean_k[k] = 0
         for l in range(1, k+1):
-            p1_k[k] += hist[l]
-            mean_k[k] += l*hist[l]
+            p1_k[k] += histogram[l]
+            mean_k[k] += l*histogram[l]
 
     mean_global = mean_k[255]
 
     between_class_variances = {}
     for k in range(1, 256):
-        try:
-            between_class_variances[k] = (mean_global * p1_k[k] - mean_k[k])**2 / (p1_k[k] * (1 - p1_k[k]))
-        except:
-            print(k)
+        if p1_k[k] != 0 and p1_k[k] != 1:
+            between_class_variances[k] = float(pow(mean_global * p1_k[k] - mean_k[k], 2)) / (p1_k[k] * (1 - p1_k[k]))
+        else:
+            between_class_variances[k] = 0
 
+    print(between_class_variances)
     k_max = 0
     var_max = 0
     for k in range(1, 256):
