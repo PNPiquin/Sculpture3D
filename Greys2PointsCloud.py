260527457835aa0a8a968189d87a6f182370c4ec
Unknown <pierre-nicolas.piquin@mines-paristech.fr>
2018-10-11 15:50:03 +0200

    Add color support in point cloud creation

--- Greys2PointsCloud.py
+++ Greys2PointsCloud.py
@@ -4,6 +4,7 @@ Created on Tue Oct  9 09:27:27 2018
 
 @author: tuoab
 """
+import cv2
 
 
 focalLength = 1
@@ -12,20 +13,29 @@ centerY = 51
 scalingFactor = 1.5
 
 
-def generate_pointcloud(depth_img, output_file='cloudpoint.txt'):
-    points = []    
+def generate_pointcloud(depth_img, output_file='cloudpoint.txt', image_file=None):
+    points = []
+    if image_file:
+        color_img = cv2.imread(image_file)
     for v in range(len(depth_img)):
         for u in range(len(depth_img[0])):
             if depth_img[v][u] > 245:
                 continue
             z = depth_img[v][u] / scalingFactor
-            points += [[u, v, z]]
+            if image_file:
+                points += [[u, v, z, color_img[v][u][0], color_img[v][u][1], color_img[v][u][2]]]
+            else:
+                points += [[u, v, z]]
 
     with open(output_file, 'w') as f:
         for i in range(len(points)):
             point = points[i]
-            for j in range(3):
-                f.write(str(point[j]) + " ")
+            if image_file:
+                for j in range(6):
+                    f.write(str(point[j]) + " ")
+            else:
+                for j in range(3):
+                    f.write(str(point[j]) + " ")
             f.write("\n")
             
     return points
